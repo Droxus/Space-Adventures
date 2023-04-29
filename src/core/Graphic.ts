dc700e170d00fe52740a490c4287126eb138b56d
sergioness <serhiikrasnovidov@gmail.com>
2023-04-29 02:38:05 +0100

    docs(core): update graphic

--- src/core/Graphic.ts
+++ src/core/Graphic.ts
@@ -1,12 +1,20 @@
-import { BufferGeometry, Material, Mesh, Object3D } from "three";
+import * as THREE from "three";
 
+/**
+ * The smallest building block of the game.
+ * Extend this class to let your entity be added to the {@link World} scene graph.
+ */
 export class Graphic {
-    private node: Object3D;
-    public constructor(params: { material: Material; geometry: BufferGeometry}) {
-        const { material, geometry } = params;
-        this.node = new Mesh(geometry, material);
+    private node: THREE.Object3D;
+    public constructor(params?: { node?: THREE.Object3D, material?: THREE.Material; geometry?: THREE.BufferGeometry}) {
+        const { node, material, geometry } = params ?? {};
+        this.node = node ?? new THREE.Mesh(geometry, material);
     }
-    public getNode(): Object3D {
+    /**
+     * Access the node object responsible for holding the geometry, material, etc.
+     * @returns this object's THREE.js representation of the node in the THREE.js scene graph
+     */
+    public getNode(): THREE.Object3D {
         return this.node;
     }
 }
