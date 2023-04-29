7b1031fc08c29e2606b8c408f6f44a4d60b6d89a
sergioness <serhiikrasnovidov@gmail.com>
2023-04-29 21:45:14 +0100

    feat(core): introduce game class
    Co-authored-by: Droxus <Droxus@users.noreply.github.com>

---
+++ src/core/Game.ts
@@ -0,0 +1,20 @@
+import { Factory } from './Factory';
+import './style.css'
+
+export class Game {
+    constructor(){
+        const controls = Factory.createControls();
+        const canvasQuery = '#canvas';
+        const canvas = document.querySelector(canvasQuery) as HTMLElement;
+        const view = Factory.createView(canvas, controls);
+        const world = Factory.createWorld();
+        view.render(world);
+        const geometry = { width: 1, height: 1, depth: 1 };
+        const material = { color: 0x00ff00 };
+        const cube = Factory.createBox({ geometry, material })
+        world.getMainGroup().add(cube);
+        controls.getCamera().position.z = 5;
+        view.render(world);
+    }
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
-import { Factory } from './core/Factory';
 import './style.css'
+import { Game } from './core/Game';
+import { Factory } from './core/Factory';
 
-const controls = Factory.createControls();
-const canvasQuery = '#canvas';
-const canvas = document.querySelector(canvasQuery) as HTMLElement;
-const view = Factory.createView(canvas, controls);
-const world = Factory.createWorld();
-view.render(world);
-const geometry = { width: 1, height: 1, depth: 1 };
-const material = { color: 0x00ff00 };
-const cube = Factory.createBox({ geometry, material })
-world.getMainGroup().add(cube);
-controls.getCamera().position.z = 5;
-view.render(world);
+const game = new Game();
