392094d2c0994cfd5a3d155e3671862a6b1a21b8
sergioness <serhiikrasnovidov@gmail.com>
2023-04-30 14:16:48 +0100

    fix(core): use resize observer to listen to canvas size changes

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,31 +1,66 @@
+import { Controls } from './Controls';
 import { Factory } from './Factory';
+import { View } from './View';
+import { World } from './World';
+
+enum StateFlag {
+    NONE,
+    STARTED,
+    PAUSED,
+    ENDED
+}
 
 export class Game {
-    public start(){
-        const controls = Factory.createControls();
-        const canvasQuery = '#canvas';
-        const canvas = document.querySelector(canvasQuery) as HTMLElement;
-        const view = Factory.createView(canvas, controls);
-        const world = Factory.createWorld();
-        view.render(world);
-
-        const geometry = { width: 1, height: 1, depth: 1 };
-        const material = { color: 0x00ff00 };
-        const cube = Factory.createBox({ geometry, material })
-        world.getMainGroup().add(cube);
-        controls.getCamera().position.z = 5;
-        view.render(world);
-
-        let needUpdate = true // need condition, maybe like wold == worldBefore
-        window.addEventListener('resize', () => {view.render(world)})
-        cube.getNode().position.z = -10;
-        function animationFrame(){
-            if (needUpdate){
-                view.render(world);
-                needUpdate = false
-            }
-            requestAnimationFrame(animationFrame)
-        }
-        animationFrame()
+    private domElement: HTMLElement;
+    private controls: Controls;
+    private view: View;
+    private world: World;
+    private state: StateFlag
+    private domElementSizeObserver: ResizeObserver;
+    constructor() {
+        this.state = StateFlag.NONE;
+        this.domElement = document.querySelector<HTMLElement>('#canvas') ??
+            document.body.appendChild(document.createElement('canvas'));
+        this.controls = Factory.createControls();
+        this.view = Factory.createView({ domElement: this.domElement, controls: this.controls });
+        this.world = Factory.createWorld();
+        this.domElementSizeObserver = new ResizeObserver(this._onDomElementSizeChanged.bind(this));
+    }
+    public start() {
+        this.state = StateFlag.STARTED;
+        this._initScene();
+        this._observeDomElementSizeChange();
+    }
+    public end(): void {
+        this.state = StateFlag.ENDED;
+        this._unobserveDomElementSizeChange();
+        this._destroyScene();
+    }
+    private _initScene(): void {
+        const boxGeometry = { width: 1, height: 1, depth: 1 };
+        const boxMaterial = { color: 0x00ff00 };
+        const box = Factory.createBox({ geometry: boxGeometry, material: boxMaterial })
+        this.world.getMainGroup().add(box);
+        this.controls.getCamera().position.z = 5;
+        this.view.render(this.world);
+    }
+    /** @todo implement */
+    private _destroyScene(): void {
+        // TODO
+    }
+    private _observeDomElementSizeChange(): void {
+        this.domElementSizeObserver.observe(this.domElement, { box: 'content-box' });
+    }
+    private _unobserveDomElementSizeChange(): void {
+        this.domElementSizeObserver.unobserve(this.domElement);
+    }
+    private _onDomElementSizeChanged([{ devicePixelContentBoxSize, contentBoxSize, contentRect }]: ResizeObserverEntry[]): void {
+        const dpr = devicePixelContentBoxSize ? 1 : window.devicePixelRatio;
+        const boxSize = devicePixelContentBoxSize?.[0] ?? contentBoxSize?.[0] ?? contentBoxSize;
+        let width = boxSize?.inlineSize ?? contentRect.width;
+        let height = boxSize?.blockSize ?? contentRect.height;
+        width = Math.round(width * dpr);
+        height = Math.round(height * dpr);
+        this.view.setSize({ width, height });
     }
 }
