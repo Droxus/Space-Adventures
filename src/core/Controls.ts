984e62c32f8c9caaf177f1b44081ccb35b210663
Droxus <alexredov@gmail.com>
2023-05-13 23:23:00 +0300

    Co-authored-by: Serhii Krasnovidov <serhiikrasnovidov@gmail.com>

--- src/core/Controls.ts
+++ src/core/Controls.ts
@@ -4,101 +4,111 @@ import { Game } from "./Game";
  * An instance of this class perceives a user's actions within the game,
  * and this the game input.
  */
+type ListenerType = (...args: any[]) => any;
 export class Controls {
-    private camera: THREE.PerspectiveCamera;
-    private euler: THREE.Euler;
-    private controlsSpeed: number;
-    private sensitivity: number;
-    private canvas: HTMLCanvasElement = (document.querySelector('#canvas') as HTMLCanvasElement);
-    public cameraSpeed: THREE.Vector3;
-    public constructor() {
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.euler = new THREE.Euler( 0, 0, 0, 'YXZ' );
-        this.controlsSpeed = 1;
-        this.sensitivity = 1;
-        this.cameraSpeed = new THREE.Vector3(0, 0, 0);
-        this.createControls()
+  private camera: THREE.PerspectiveCamera;
+  private euler: THREE.Euler;
+  private controlsSpeed: number;
+  private sensitivity: number;
+  private canvas: HTMLCanvasElement = document.querySelector(
+    "#canvas"
+  ) as HTMLCanvasElement;
+  public cameraSpeed: THREE.Vector3;
+  private listeners: ListenerType[] = [];
+  public constructor() {
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.euler = new THREE.Euler(0, 0, 0, "YXZ");
+    this.controlsSpeed = 1;
+    this.sensitivity = 1000;
+    this.cameraSpeed = new THREE.Vector3(0, 0, 0);
+    this.createControls();
+  }
+  public getCamera(): THREE.Camera {
+    return this.camera;
+  }
+  /**
+   * Update this this within the {@link context} specified.
+   * @param context a context object to update the this with
+   */
+  public update(context: {
+    viewport: { width: number; height: number };
+  }): void {
+    const { width, height } = context.viewport;
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  }
+  private _onDomElementClick(): void {
+    this.canvas.requestPointerLock();
+  }
+  private createControls(): void {
+    this.canvas.addEventListener("click", this._onDomElementClick.bind(this));
+    window.addEventListener("keydown", this._onKeyDown.bind(this));
+    window.addEventListener("keyup", this._onKeyUp.bind(this));
+    this.canvas.addEventListener("mousemove", this._onMouseMove.bind(this));
+  }
+  private keydowns = new Map([
+    ["KeyW", () => (this.cameraSpeed.z = -1)],
+    ["KeyA", () => (this.cameraSpeed.x = -1)],
+    ["KeyS", () => (this.cameraSpeed.z = 1)],
+    ["KeyD", () => (this.cameraSpeed.x = 1)],
+    ["Space", () => (this.cameraSpeed.y = 1)],
+    ["ShiftLeft", () => (this.cameraSpeed.y = -1)],
+  ]);
+  private keyup = new Map([
+    ["KeyW", () => (this.cameraSpeed.z = Math.max(0, this.cameraSpeed.z))],
+    ["KeyA", () => (this.cameraSpeed.x = Math.max(0, this.cameraSpeed.x))],
+    ["KeyS", () => (this.cameraSpeed.z = Math.min(0, this.cameraSpeed.z))],
+    ["KeyD", () => (this.cameraSpeed.x = Math.min(0, this.cameraSpeed.x))],
+    ["Space", () => (this.cameraSpeed.y = Math.min(0, this.cameraSpeed.y))],
+    ["ShiftLeft", () => (this.cameraSpeed.y = Math.max(0, this.cameraSpeed.y))],
+  ]);
+  public translateCameraTo(
+    params: Partial<{ x: number; y: number; z: number }>
+  ): void {
+    const { x, y, z } = params;
+    x && this.camera.translateX(x);
+    y && this.camera.translateY(y);
+    z && this.camera.translateZ(z);
+    const fireListeners = x || y || z;
+    fireListeners && this._fireChangeEvent();
+  }
+  private _onKeyDown(event: KeyboardEvent): void {
+    if (this.keydowns.has(event.code)) {
+      const processThisEventCode = this.keydowns.get(event.code);
+      if (processThisEventCode) processThisEventCode();
     }
-    public getCamera(): THREE.Camera {
-        return this.camera;
+    this._fireChangeEvent();
+  }
+  private _onKeyUp(event: KeyboardEvent): void {
+    if (this.keyup.has(event.code)) {
+      const processThisEventCode = this.keyup.get(event.code);
+      if (processThisEventCode) processThisEventCode();
     }
-    /**
-     * Update this this within the {@link context} specified.
-     * @param context a context object to update the this with
-     */
-    public update(context: { viewport: { width: number, height: number } }): void {
-        const { width, height } = context.viewport;
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
-    }
-    private _onDomElementClick(): void {
-        this.canvas.requestPointerLock();
-    }
-    private createControls(): void {
-        // flythis for comfortable devloping   
-        document.addEventListener('pointerlockchange', () => {console.log('Pointer has been changed')}, false);
-        this.canvas.addEventListener('click', this._onDomElementClick.bind(this));
-        window.addEventListener('keydown', this._onKeyDown.bind(this))
-        window.addEventListener('keyup', this._onKeyUp.bind(this))
-        this.canvas.addEventListener('mousemove', this._onMouseMove.bind(this))
-    }
-    private keydowns = new Map([
-        ['KeyW', () => this.cameraSpeed.z = -1],
-        ['KeyA', () => this.cameraSpeed.x = -1],
-        ['KeyS', () => this.cameraSpeed.z = 1],
-        ['KeyD', () => this.cameraSpeed.x = 1],
-        ['Space', () => this.cameraSpeed.y = 1],
-        ['ShiftLeft', () => this.cameraSpeed.y = -1],
-    ]);
-    private keyup = new Map([
-        ['KeyW', () => this.cameraSpeed.z = Math.max(0, this.cameraSpeed.z)],
-        ['KeyA', () => this.cameraSpeed.x = Math.max(0, this.cameraSpeed.x)],
-        ['KeyS', () => this.cameraSpeed.z = Math.min(0, this.cameraSpeed.z)],
-        ['KeyD', () => this.cameraSpeed.x = Math.min(0, this.cameraSpeed.x)],
-        ['Space', () => this.cameraSpeed.y = Math.min(0, this.cameraSpeed.y)],
-        ['ShiftLeft', () => this.cameraSpeed.y = Math.max(0, this.cameraSpeed.y)],
-    ]);
-    private translateCameraTo(params: Partial<{ x: number, y: number, z: number }>): void {
-        const { x, y, z } = params;
-        x && this.camera.translateX(x);
-        y && this.camera.translateY(y);
-        z && this.camera.translateZ(z);
-    }
-    private _onKeyDown(event: KeyboardEvent): void {
-        if (this.keydowns.has(event.code)){
-            const processThisEventCode = this.keydowns.get(event.code);
-            if (processThisEventCode) processThisEventCode();
-        }
-        this._fireChangeEvent()
-    }
-    private _onKeyUp(event: KeyboardEvent): void {
-        if (this.keyup.has(event.code)){
-            const processThisEventCode = this.keyup.get(event.code);
-            if (processThisEventCode) processThisEventCode();
-        }
-    }
-    private _fireChangeEvent(): void {
-        Game.makeRender()
-        // call the callback received in the addListener(callback) method of this class,
-        // which would make the Game class's instance to re-render its scene
-    }
-    private _onMouseMove(event: MouseEvent): void {
-            const movementX = event.movementX || 0;
-            const movementY = event.movementY || 0;
+  }
+  private _fireChangeEvent(): void {
+    this.listeners.forEach((listener) => listener());
+  }
+  public addListener(listener: ListenerType): void {
+    this.listeners.push(listener);
+  }
+  public removeListener(listener: ListenerType): void {
+    let index = this.listeners.findIndex((element) => element == listener);
+    index > -1 && this.listeners.splice(index, 1);
+  }
+  private _onMouseMove(event: MouseEvent): void {
+    const movementX = event.movementX || 0;
+    const movementY = event.movementY || 0;
 
-            this.euler.x -= movementY / (1 / this.sensitivity * 1000)
-            this.euler.y -= movementX / (1 / this.sensitivity * 1000) 
-        
-            this.euler.x = Math.max(Math.min(Math.PI/2, this.euler.x), -Math.PI/2)
-            this.camera.quaternion.setFromEuler( this.euler );
-            this._fireChangeEvent()
-    }
-    public makeObjectMove(params: {obj: THREE.Object3D, diffPosition: {x: number, y: number, z: number}}){
-        this.translateCameraTo({ x: params.diffPosition.x * this.controlsSpeed})
-        this.translateCameraTo({ y: params.diffPosition.y * this.controlsSpeed})
-        this.translateCameraTo({ z: params.diffPosition.z * this.controlsSpeed})
-        if (params.diffPosition.x || params.diffPosition.y || params.diffPosition.z){
-            this._fireChangeEvent()
-        }
-    }
+    this.euler.x -= movementY / this.sensitivity;
+    this.euler.y -= movementX / this.sensitivity;
+
+    this.euler.x = Math.max(Math.min(Math.PI / 2, this.euler.x), -Math.PI / 2);
+    this.camera.quaternion.setFromEuler(this.euler);
+    this._fireChangeEvent();
+  }
 }
